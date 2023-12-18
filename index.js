77e1905fc864d2c489fba26d1a77615a1b9f9cf1
praveen <praveenmech483@gmail.com>
2023-12-18 16:40:47 +0530

    start the express server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('./utils/config')
+const app = require('./app')
 console.log('conneting to mongodb')
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
         console.log('connected to mongodb')
+
+        app.listen(config.PORT, () => {
+            console.log(`server runing on port ${config.PORT}`)
+        })
     })
     .catch((error) => {
         console.log('error connection to mongodb', error.message)
